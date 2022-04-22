0174db93e3088510459ca382248840a020e67d33
Raman Pandey <102546029+R-A-M-A-N-P-A-N-D-E-Y@users.noreply.github.com>
2022-04-22 23:11:00 +0530

    Update AddMatrix.c

--- AddMatrix.c
+++ AddMatrix.c
@@ -1,24 +1,72 @@
+/** Addition Of two Matrix */
+
 #include<stdio.h>
 int main()
 {
-    int m, n, c, d, first[10][10], second[10][10], sum[10][10];
-    printf("Enter the number of rows and columns of matrix\n");
-    scanf("%d%d", & m, & n);
-    printf("Enter the elements of first matrix\n");
-    for (c = 0; c < m; c++)
-        for (d = 0; d < n; d++) scanf("%d", & first[c][d]);
-    printf("Enter the elements of second matrix\n");
-    for (c = 0; c < m; c++)
-        for (d = 0; d < n; d++) scanf("%d", & second[c][d]);
-    printf("Sum of entered matrices:-\n");
-    for (c = 0; c < m; c++)
+    int i,j,n,m,x,y;
+    printf("Program to Find Sum of Matrices :\n\n");
+
+    printf("Enter The Range Of the First Matrix :\n");
+    scanf("%d %d",&n,&m);
+    int arr[n][m];
+
+    printf("\nEnter The Elements Of The First Matrix :\n");
+    for(i=0; i<n; i++)
     {
-        for (d = 0; d < n; d++)
+        for(j=0; j<m; j++)
         {
-            sum[c][d] = first[c][d] + second[c][d];
-            printf("%d\t", sum[c][d]);
+            scanf("%d",&arr[i][j]);
         }
-        printf("\n");
     }
+
+    printf("\nEnter The Range Of the Second Matrix :\n");
+    scanf("%d%d",&x,&y);
+    int arr1[x][y];
+
+    printf("\nEnter The Elements Of The Second Matrix :\n");
+    for(i=0; i<x; i++)
+    {
+        for(j=0; j<y; j++)
+        {
+            scanf("%d",&arr1[i][j]);
+        }
+    }
+
+    printf("\n\nFirst Matrix Entered :\n");
+    for(i=0; i<n; i++)
+    {
+        for(j=0; j<m; j++)
+        {
+            printf("%d\t",arr[i][j]);
+        }printf("\n");
+    }
+
+    printf("\n\nSecond Matrix Entered :\n");
+    for(i=0; i<x; i++)
+    {
+        for(j=0; j<y; j++)
+        {
+            printf("%d\t",arr1[i][j]);
+        }printf("\n");
+    }
+
+    if(n != x && m != y)
+    {
+        printf("\n\nAddition Cannot be Done\n\n");
+    }
+    else
+    {
+        int sum[n][m];
+        printf("\nSum Of Two Matrix is :\n");
+        for(i=0; i<n; i++)
+        {
+            for(j=0; j<y; j++)
+            {
+                sum[i][j] = arr[i][j] + arr1[i][j];
+                printf("%d\t", sum[i][j]);
+            }printf("\n");
+        }
+    }
+
     return 0;
 }
